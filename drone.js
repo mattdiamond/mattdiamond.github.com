3142ff989c44e28bb76002a1a30fa3f29fcd4a63
Matt Diamond <mdiamond@jhu.edu>
2012-04-20 17:19:27 -0700

    updates

--- drone.js
+++ drone.js
@@ -9,18 +9,15 @@ for (var i = 0; i < bufferData.length; i++){
 	bufferData[i] = samp;
 }
 
-var panners = [];
-
 function createNoiseGen(freq){
 	var panner = context.createPanner();
 	var max = 20;
 	var min = -20;
-	var x = Math.random() * (max - min) + min;
-	var y = Math.random() * (max - min) + min;
-	var z = Math.random() * (max - min) + min;
+	var x = rand(min, max);
+	var y = rand(min, max);
+	var z = rand(min, max);
 	panner.setPosition(x, y, z);
 	panner.connect(context.destination);
-	panners.push(panner);
 	
 	var filter = context.createBiquadFilter();
 	filter.type = 2; //bandpass
@@ -33,6 +30,14 @@ function createNoiseGen(freq){
 	bufferSource.buffer = buffer;
 	bufferSource.connect(filter);
 	bufferSource.gain = 0.2;
+	bufferSource.playbackRate.value = 1 + rand(-0.1, 0.1);
+	
+	setInterval(function(){
+		x = x + rand(-0.5, 0.5);
+		y = y + rand(-0.5, 0.5);
+		z = z + rand(-0.5, 0.5);
+		panner.setPosition(x, y, z);
+	}, 500);
 	
 	bufferSource.noteOn(0);
 }
@@ -50,4 +55,8 @@ for (var i = 0; i < num_osc; i++){
 
 function mtof(m){
 	return Math.pow(2, (m-69)/12) * 440;
+}
+
+function rand(min, max){
+	return Math.random() * (max - min) + min;
 }
