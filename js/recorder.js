4cdc44d767b8b62201b63c131703b60fb144e748
Matt Diamond <mdiamond@onehealth.com>
2012-09-13 16:10:58 -0700

    adding recorder files back in

---
+++ js/recorder.js
@@ -0,0 +1,64 @@
+(function(window){
+
+  var WORKER_PATH = 'js/recorderjs/recorderWorker.js';
+
+  var Recorder = function(source, cfg){
+    var config = cfg || {};
+    var bufferLen = config.bufferLen || 4096;
+    this.context = source.context;
+    this.node = this.context.createJavaScriptNode(bufferLen, 2, 2);
+    var worker = new Worker(WORKER_PATH);
+    worker.postMessage({
+      command: 'init',
+      config: {
+        sampleRate: this.context.sampleRate
+      }
+    });
+    var recording = false,
+        currCallback;
+
+    this.node.onaudioprocess = function(e){
+      if (!recording) return;
+      worker.postMessage({
+        command: 'record',
+        buffer: [
+          e.inputBuffer.getChannelData(0),
+          e.inputBuffer.getChannelData(1)
+        ]
+      });
+    }
+
+    this.configure = function(cfg){
+      config = cfg;
+    }
+
+    this.record = function(){
+      recording = true;
+    }
+
+    this.stop = function(){
+      recording = false;
+    }
+
+    this.clear = function(){
+      worker.postMessage({ command: 'clear' });
+    }
+
+    this.exportWAV = function(cb){
+      currCallback = cb || config.callback;
+      if (!currCallback) throw new Error('Callback not set');
+      worker.postMessage({ command: 'exportWAV' });
+    }
+
+    worker.onmessage = function(e){
+      var blob = e.data;
+      currCallback(blob);
+    }
+
+    source.connect(this.node);
+    this.node.connect(this.context.destination);    //this should not be necessary
+  };
+
+  window.Recorder = Recorder;
+
+})(window);
---
+++ js/recorderWorker.js
@@ -0,0 +1,80 @@
+importScripts('pcmdata.min.js');
+
+var recLength = 0,
+    recBuffers = [],
+    sampleRate;
+
+this.onmessage = function(e){
+  switch(e.data.command){
+    case 'init':
+      init(e.data.config);
+      break;
+    case 'record':
+      record(e.data.buffer);
+      break;
+    case 'exportWAV':
+      exportWAV();
+      break;
+    case 'clear':
+      clear();
+      break;
+  }
+};
+
+function init(config){
+  sampleRate = config.sampleRate;
+}
+
+function record(inputBuffer){
+  var bufferL = inputBuffer[0];
+  var bufferR = inputBuffer[1];
+  var interleaved = interleave(bufferL, bufferR);
+  recBuffers.push(interleaved);
+  recLength += interleaved.length;
+}
+
+function exportWAV(){
+  var buffer = mergeBuffers(recBuffers, recLength);
+  var waveData = PCMData.encode({
+    sampleRate:     sampleRate,
+    channelCount:   2,
+    bytesPerSample: 2,    //16bit
+    data:           buffer
+  });
+  var byteArray = new Uint8Array(waveData.length);
+  for (var i = 0; i < waveData.length; i++){
+    byteArray[i] = waveData.charCodeAt(i);
+  }
+  var audioBlob = new Blob([byteArray], { type: 'audio/wav' });
+  this.postMessage(audioBlob);
+}
+
+function clear(){
+  recLength = 0;
+  recBuffers = [];
+}
+
+function mergeBuffers(recBuffers, recLength){
+  var result = new Float32Array(recLength);
+  var offset = 0;
+  for (var i = 0; i < recBuffers.length; i++){
+    result.set(recBuffers[i], offset);
+    offset += recBuffers[i].length;
+  }
+  return result;
+}
+
+function interleave(inputL, inputR){
+  var length = inputL.length + inputR.length;
+  var result = new Float32Array(length);
+
+  var index = 0,
+    inputIndex = 0;
+
+  while (index < length){
+    result[index++] = inputL[inputIndex];
+    result[index++] = inputR[inputIndex];
+    inputIndex++;
+  }
+  return result;
+}
